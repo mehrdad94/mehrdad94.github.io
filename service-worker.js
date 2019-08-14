12c3d1062167be64408c495d46bc1f187a0f3807
Mehrdad <m_javidi@outlook.com>
2019-08-14 20:41:25 +0430

    update package

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.151c6b72b4632ebed8904a8f2bc0ed8a.js"
+  "/precache-manifest.fa8c889c624de5799bea4bc165537d55.js"
 );
 
 self.addEventListener('message', (event) => {
